ae3892cd1270ac4b6eced56ad4aa3230bb14550f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-07-24 12:30:57 +0300

    final

---
+++ window functions.sql
@@ -0,0 +1,65 @@
+-- This can show how the total sales amount accumulates over time --
+SELECT TOP (1000) [Order Numer]
+      ,[Quantity]
+      ,[ Unit Price ]
+      ,[ Total Amount ]
+      ,[Transaction Date]
+      ,[Shipping Status]
+      ,[Year of Transaction]
+      ,[Product Type]
+      ,[Product Code]
+      ,[Customer Name]
+      ,[Customer ID]
+      ,[Transaction ID],
+	  SUM([ Total Amount ]) OVER (ORDER BY [Transaction Date]) AS RunningTotalAmount 
+  FROM [Kaptagat Springs Sales Data].[dbo].['Kaptagat Springs Sales Datasets$']
+
+--  This will rank sales by the total amount within each year. --
+  SELECT TOP (1000) [Order Numer]
+      ,[Quantity]
+      ,[ Unit Price ]
+      ,[ Total Amount ]
+      ,[Transaction Date]
+      ,[Shipping Status]
+      ,[Year of Transaction]
+      ,[Product Type]
+      ,[Product Code]
+      ,[Customer Name]
+      ,[Customer ID]
+      ,[Transaction ID],
+	 RANK() OVER (PARTITION BY [Year of Transaction] ORDER BY [ Total Amount ] DESC) AS SalesRank
+  FROM [Kaptagat Springs Sales Data].[dbo].['Kaptagat Springs Sales Datasets$']
+
+
+--- This provides the average sales amount for each product type. 
+  SELECT TOP (1000) [Order Numer]
+      ,[Quantity]
+      ,[ Unit Price ]
+      ,[ Total Amount ]
+      ,[Transaction Date]
+      ,[Shipping Status]
+      ,[Year of Transaction]
+      ,[Product Type]
+      ,[Product Code]
+      ,[Customer Name]
+      ,[Customer ID]
+      ,[Transaction ID],
+	   AVG([ Total Amount ]) OVER (PARTITION BY [Product Type]) AS AvgSalesByProductType
+  FROM [Kaptagat Springs Sales Data].[dbo].['Kaptagat Springs Sales Datasets$']
+
+
+-- This calculates the difference in total amount from the previous transaction.
+  SELECT TOP (1000) [Order Numer]
+      ,[Quantity]
+      ,[ Unit Price ]
+      ,[ Total Amount ]
+      ,[Transaction Date]
+      ,[Shipping Status]
+      ,[Year of Transaction]
+      ,[Product Type]
+      ,[Product Code]
+      ,[Customer Name]
+      ,[Customer ID]
+      ,[Transaction ID],
+ [ Total Amount ] - LAG([ Total Amount ], 1, 0) OVER (ORDER BY [Transaction Date]) AS DifferenceFromPreviousTransaction
+  FROM [Kaptagat Springs Sales Data].[dbo].['Kaptagat Springs Sales Datasets$']
