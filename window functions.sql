812cf8abfe1e51a56a9ea6507ddd5a5bc0c4819e
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-07-25 16:48:46 +0300

    final

--- window functions.sql
+++ window functions.sql
@@ -40,3 +40,22 @@ FROM [Kaptagat Springs Sales Data].[dbo].['Kaptagat Springs Sales Datasets$'];
   DENSE_RANK() OVER (PARTITION BY [Customer ID] ORDER BY [ Total Amount ]
  DESC) AS DenseRankByCustomer
 FROM [Kaptagat Springs Sales Data].[dbo].['Kaptagat Springs Sales Datasets$'];
+
+
+--  Divide the transactions into 4 quartiles for each customer based on transaction amount. --
+  SELECT TOP (1000) 
+    [Order Numer],
+    [Quantity],
+    [ Unit Price ],
+    [ Total Amount ],
+    [Transaction Date],
+    [Shipping Status],
+    [Year of Transaction],
+    [Product Type],
+    [Product Code],
+    [Customer Name],
+    [Customer ID],
+    [Transaction ID],
+   NTILE(4) OVER (PARTITION BY [Customer ID] ORDER BY  [ Total Amount ]DESC) AS QuartileByCustomer
+FROM [Kaptagat Springs Sales Data].[dbo].['Kaptagat Springs Sales Datasets$'];
+
